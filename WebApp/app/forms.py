270c493b01138372d5ca7f6c78e2e43f97bd7b8c
Marco Belotti <m.belotti20@campus.unimib.it>
2019-04-03 21:47:45 +0200

    Struttura form WebApp

---
+++ WebApp/app/forms.py
@@ -0,0 +1,91 @@
+# coding=utf-8
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
+    TextAreaField, SelectField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, \
+    Length
+from app.models import User
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    cellular = StringField('Cellular', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+			
+    def validate_cellular(self, cellular):
+        user = User.query.filter_by(cellular=cellular.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different cellular number.')
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class MessageForm(FlaskForm):
+    message = TextAreaField('Message', validators=[
+        DataRequired(), Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+class PianoForm(FlaskForm):
+    topic = StringField('Topic', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+	
+class StanzaForm(FlaskForm):
+    piano = SelectField('Piano:', coerce=int, validators=[DataRequired()], id='select_piano')
+    topic = StringField('Topic', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+	
+class DispositivoForm(FlaskForm):
+    piano = SelectField('Piano:', coerce=int, validators=[DataRequired()], id='select_piano')
+    stanza = SelectField('Stanza:', coerce=int, validators=[DataRequired()], id='select_stanza')
+    tipo = SelectField('Tipologia:', coerce=int, validators=[DataRequired()], id='select_dispositivo')
+    pin = StringField('Pin:', validators=[DataRequired()])
+    topic = StringField('Topic', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')

acccf7cf026b030cf3e903a3f2b67b6b6621272c
Marco Belotti <m.belotti20@campus.unimib.it>
2019-04-03 12:24:47 +0000

    Revert "Flask Web App e DB SQLite"
    This reverts commit e8f8499ab25da10decc2b90d31dc6f90145990b4

--- SmartHomeAutomation/smartHome.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-from app import app, db
-from app.models import User, Post
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
-	
-app.run( host = '0.0.0.0' , debug = False )

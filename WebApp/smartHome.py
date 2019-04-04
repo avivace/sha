eea79d9b31e0d8bf862235cbe4e9a098b450c961
Marco Belotti <m.belotti20@campus.unimib.it>
2019-04-04 13:32:10 +0200

    Aggiornamento shell context

--- WebApp/smartHome.py
+++ WebApp/smartHome.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 from app import app, db
-from app.models import User, Post
+from app.models import User, Post, Notification, Message
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
-	
+    return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
+            'Notification': Notification}
+
 app.run( host = '0.0.0.0' , debug = False )

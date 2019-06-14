b8a66bc54c1120f7272073a3f00c9c5cfce85e61
Antonio Vivace <avivace4@gmail.com>
2019-06-14 23:41:21 +0200

    Add /get_topics and MQTT publish routes

--- backend/api.py
+++ backend/api.py
@@ -3,11 +3,14 @@ from models import User, Post, Piano, Stanza, Attuatore, Sensore, Message, Notif
 import time
 from jose import JWTError, jwt
 from app import db
+import paho.mqtt.client as mqttc
 
 JWT_ISSUER = 'smarthomeautomation'
 JWT_SECRET = 'asdfasdfasdfasdfasdfasdfasdfasdfasdfa'
 JWT_LIFETIME_SECONDS = 600
 JWT_ALGORITHM = 'HS256'
+
+
 
 
 # Utilities
@@ -65,4 +68,33 @@ def add_device():
                        stanza_id=1)
     db.session.add(device)
     db.session.commit()
+    return "OK"
+
+# Restituisce l'elenco di tutti i topic associati ad attuatori di tipo lampada
+def get_topics():
+    piani = Piano.query.all()
+    topicArray = []
+
+    for piano in piani:
+        stanze = Stanza.query.filter_by(piano_id=piano.id).all()
+        for stanza in stanze:
+        # Aggiungere elenco attuatori sia di tipo "lampada" che di tipo "serratura"
+        # Da vedere come costruire la query con operatore di OR
+            attuatori = Attuatore.query.filter_by(stanza_id=stanza.id, type='lampada')
+            for attuatore in attuatori:
+                topicObj = {}
+                topicObj['id'] = attuatore.id
+                topicObj['topic'] = piano.topic + '/' + stanza.topic + '/' + attuatore.topic
+            topicArray.append(topicObj)
+
+    return topicArray
+
+def mqtt_publish(id, value):
+    att = Attuatore.query.filter_by(id=id).first()
+    stanza = Stanza.query.filter_by(id=att.stanza_id).first()
+    client = mqttc.Client()
+    client.connect("127.0.0.1", 1883, 60)
+    topic = str(stanza.piano_id) + '/' + str(stanza.id) + '/' + str(id)
+    client.publish(topic, value)
+
     return "OK"

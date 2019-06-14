11fd31727acb4a96cc33202359976508e6fa3920
Antonio Vivace <avivace4@gmail.com>
2019-06-14 14:14:33 +0200

    Clean node environment, too

--- clean_venv.sh
+++ clean_venv.sh
@@ -1,3 +1,8 @@
+# This resets everything related to the deployed instance
+# It cleans installed Node and Python dependencies and deletes the virtual env
+# You need to rerun the deploy commands after this 
+# (create venv, activate it, install python dependencies, install node dependencies)
+
 cd backend
 rm -rf bin
 rm -rf include
@@ -6,3 +11,5 @@ rm lib64
 rm -rf share
 rm pyvenv.cfg
 rm -rf __pycache__
+cd frontend
+rm node_modules

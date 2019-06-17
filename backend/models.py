8b1dd92e2485bfc51f96723f48c00243fc5462b0
Antonio Vivace <avivace4@gmail.com>
2019-06-18 01:06:31 +0200

    Remove unused methods, add update attuatore

--- backend/models.py
+++ backend/models.py
@@ -7,12 +7,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 from hashlib import sha512
 
-followers = db.Table(
-    'followers',
-    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-)
-
 def hash_password(password):
     salt = "1234780asa231"
     fullstring = password + salt;
@@ -27,24 +21,7 @@ class User(db.Model):
     is_active = db.Column(db.Boolean, index=True, unique=False)
     password_hash = db.Column(db.String(128))
 
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
-
     about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-
-    followed = db.relationship(
-        'User', secondary=followers,
-        primaryjoin=(followers.c.follower_id == id),
-        secondaryjoin=(followers.c.followed_id == id),
-        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
-
-    messages_sent = db.relationship('Message',
-                                    foreign_keys='Message.sender_id',
-                                    backref='author', lazy='dynamic')
-    messages_received = db.relationship('Message',
-                                        foreign_keys='Message.recipient_id',
-                                        backref='recipient', lazy='dynamic')
-    last_message_read_time = db.Column(db.DateTime)
 
     notifications = db.relationship('Notification', backref='user',
                                     lazy='dynamic')
@@ -63,30 +40,6 @@ class User(db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
-
-    def follow(self, user):
-        if not self.is_following(user):
-            self.followed.append(user)
-
-    def unfollow(self, user):
-        if self.is_following(user):
-            self.followed.remove(user)
-
-    def is_following(self, user):
-        return self.followed.filter(
-            followers.c.followed_id == user.id).count() > 0
-
-    def followed_posts(self):
-        followed = Post.query.join(
-            followers, (followers.c.followed_id == Post.user_id)).filter(
-                followers.c.follower_id == self.id)
-        own = Post.query.filter_by(user_id=self.id)
-        return followed.union(own).order_by(Post.timestamp.desc())
-
-    def new_messages(self):
-        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
-        return Message.query.filter_by(recipient=self).filter(
-            Message.timestamp > last_read_time).count()
 
     def new_requests(self):
         return User.query.filter_by(is_active=False).count()
@@ -110,25 +63,6 @@ class User(db.Model):
         except:
             return
         return User.query.get(id)
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return '<Post {}>'.format(self.body)
-
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Message {}>'.format(self.body)
 
 class Notification(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -173,6 +107,12 @@ class Attuatore(db.Model):
         self.status = not self.status
     def __repr__(self):
         return '<Attuatore {}>'.format(self.description)
+    def update(self, description, type, pin, stanza):
+        self.description = description
+        self.type = type
+        self.pin = pin
+        self.stanza_id = stanza
+
 
 class Pulsante(db.Model):
     id = db.Column(db.Integer, primary_key=True)

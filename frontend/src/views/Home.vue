0efbba7d233b9ad740a9099ed96360fb0568864b
Antonio Vivace <avivace4@gmail.com>
2019-06-17 02:17:53 +0200

    Fancy landing page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,40 @@
 <template>
 	<div>
-		Landing Page pubblica
+		<center>
+			<br>
+			<div class="landing">
+			Smart Home Automation
+			</div>
+			<br><br>
+			<div class="subtext">
+				Laboratorio di Progettazione, A.A. 2018/2019 <br>
+				Antonio Vivace, Marco Belotti, Francesco Bombarda
+				<br><br>
+				Questa è la landing page pubblica.
+
+			</div>
+		</center>
+
 	</div>
 </template>
 
 <script>
 export default { }
 </script>
+
+<style scoped="true">
+.landing {
+	font-size: 5rem;
+	text-transform: uppercase;
+	letter-spacing: 4px;
+	font-weight: 300;
+	text-shadow: gray 0 0 0.4em;
+	line-height: 4rem;
+}
+.subtext {
+	font-size: 1.75rem;
+
+	letter-spacing: 1px;
+	font-weight: 300;
+}
+</style>
